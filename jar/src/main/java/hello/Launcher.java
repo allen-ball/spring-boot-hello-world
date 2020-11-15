a7e63c02344bdba8a88e5a428b848d1f1977c51b
Allen D. Ball <ball@hcf.dev>
2020-11-15 15:05:20 -0800

    Retain reference to ApplicationContext.

--- jar/src/main/java/hello/Launcher.java
+++ jar/src/main/java/hello/Launcher.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 
 /**
  * {@link SpringApplication} {@link Launcher}
@@ -18,10 +19,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 @NoArgsConstructor @ToString @Log4j2
 public class Launcher extends SpringBootServletInitializer {
+    private static ApplicationContext context = null;
 
     /**
-     * Standard {@link SpringApplication} {@code main(String[])} entry
-     * point.
+     * Standard {@link SpringApplication} {@code main(String[])}
+     * entry point.
      *
      * @param   argv            The command line argument vector.
      *
@@ -31,13 +33,13 @@ public class Launcher extends SpringBootServletInitializer {
     public static void main(String[] argv) throws Exception {
         SpringApplication application = new SpringApplication(Launcher.class);
 
-        log.debug("{}", () -> Arrays.toString(argv));
+        log.info("{}", () -> Arrays.toString(argv));
 
-        application.run(argv);
+        context = application.run(argv);
     }
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Launcher.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Launcher.class);
     }
 }
